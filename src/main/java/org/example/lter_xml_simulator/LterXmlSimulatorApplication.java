a47ea860f7aa37dca7ab9d95dca85c3e3cbb581c
HyoBN <qls0786@naver.com>
2024-07-04 11:13:10 +0900

    init

---
+++ src/main/java/org/example/lter_xml_simulator/LterXmlSimulatorApplication.java
@@ -0,0 +1,15 @@
+package org.example.lter_xml_simulator;
+
+import lombok.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LterXmlSimulatorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LterXmlSimulatorApplication.class, args);
+
+    }
+}
---
+++ src/test/java/org/example/lter_xml_simulator/LterXmlSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lter_xml_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LterXmlSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
